766c5e165d7d5dd124dc33cabd042502f21f1171
mailog <m.ilog@yahoo.com>
2020-02-12 14:41:36 -0800

    Add files via upload

---
+++ Unused/EnemyGroup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroup : MonoBehaviour
+{
+    public GameObject[] enemies;
+
+    public int[] difficulties;
+
+    public Vector2 xSpawnRange;
+    public float ySpawn, yRange;
+
+    public int currDiff, totalDiff;
+
+    private List<GameObject> chosenEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnGroup();
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChooseEnemies()
+    {
+        chosenEnemies = new List<GameObject>();
+        int chosenIndex;
+        while(currDiff < totalDiff)
+        {
+            chosenIndex = Random.Range(0, enemies.Length);
+            chosenEnemies.Add(enemies[chosenIndex]);
+            currDiff += difficulties[chosenIndex];
+        }
+    }
+
+    void SpawnGroup()
+    {
+        ChooseEnemies();
+        foreach(GameObject enemy in enemies)
+        {
+            Instantiate(enemy, new Vector2(Random.Range(-0.75f, 0.75f), ySpawn - Random.Range(0, yRange)), Quaternion.identity);
+        }
+    }
+}
